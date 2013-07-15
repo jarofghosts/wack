6258026351465bb5e5e5614cdf6aa57aae818af5
jesse keane <me@jessekeane.me>
2013-07-15 19:14:13 +0400

    refactoring

--- index.js
+++ index.js
@@ -8,96 +8,116 @@ var c = require('commander'),
     color = require('bash-color'),
     Readable = require('stream').Readable,
     rs = Readable(),
-    currentFile = null,
-    fileCount = 0,
-    options = {};
+    path = require('path'),
+    isCli = (require.main === module);;
 
-tr = through(write, end);
+module.exports = wack;
 
-function write(obj) {
-  if (options.exclude.length) {
-    var i = 0,
-        l = options.exclude.length;
-    for (; i < l; ++i) {
-      if (type.compare(obj.filename, options.exclude[i])) return
+function wack(options) {
+
+  var currentFile = null,
+      fileCount = 0;
+
+  options = options || {};
+  
+  if (!options.dir) options.dir = process.cwd();
+  if (!options.dir.match(/^\//)) options.dir = path.normalize(options.dir);
+
+  options.types = options.type ? options.type.replace(/\s+/, '').split(',') : [];
+  options.exclude = options.notype ? options.notype.replace(/\s+/, '').split(',') : [];
+  options.regex = options.ignorecase ? new RegExp(options.pattern, "ig") : new RegExp(options.pattern, "g");
+  
+  tr = through(write, end);
+
+  function write(obj) {
+    if (options.exclude.length) {
+      var i = 0,
+          l = options.exclude.length;
+      for (; i < l; ++i) {
+        if (type.compare(obj.filename, options.exclude[i])) return
+      }
+    }
+    if (options.types.length) {
+      var i = 0,
+          proceed = false,
+          l = options.types.length;
+      for (; i < l; ++i) {
+        if (type.compare(obj.filename, options.types[i])) proceed = true;
+      }
+      if (!proceed) return
+    }
+    var match = options.regex.exec(obj.data);
+    if ((match && !options.invertmatch) || (!match && options.invertmatch)) {
+      var filename = obj.filename.substring(options.dir.length),
+          line = obj.line + ':',
+          str = obj.data,
+          fileOut = filename,
+          toOutput = '';
+      if (options.nocolor || options.invertmatch) {
+        toOutput = ' ' + line + ' ' + str + '\n';
+      } else {
+
+        fileOut = color.green(filename);
+        var finalString = str.substring(0, match.index) + 
+                          color.yellow(match[0], true) +
+                          str.substring(match.index + match[0].length),
+            toOutput = ' ' + color.wrap(line, color.colors.WHITE, color.styles.bold) + ' ' + finalString + '\n';
+      
+      }
+
+      if (filename != currentFile) {
+        fileCount = 1;
+        currentFile = filename;
+        this.queue(fileOut + '\n');
+      } else {
+        if (options.maxcount) fileCount++;
+        if (fileCount > options.maxcount) return
+      }
+      this.queue(toOutput);
+      if (options.justone) tr.queue(null);
     }
   }
-  if (options.types.length) {
-    var i = 0,
-        proceed = false,
-        l = options.types.length;
-    for (; i < l; ++i) {
-      if (type.compare(obj.filename, options.types[i])) proceed = true;
-    }
-    if (!proceed) return
+
+  function end() {
+    this.queue(null);
   }
-  var match = options.regex.exec(obj.data);
-  if ((match && !options.invert) || (!match && options.invert)) {
-    var filename = obj.filename.replace(new RegExp('^' + options.dir), ''),
-        line = obj.line + ':',
-        str = obj.data,
-        fileOut = filename,
-        toOutput = '';
-    if (options.nocolor || options.invert) {
-      toOutput = ' ' + line + ' ' + str + '\n';
-    } else {
 
-      fileOut = color.green(filename);
-      var finalString = str.substring(0, match.index) + 
-                        color.yellow(match[0], true) +
-                        str.substring(match.index + match[0].length),
-          toOutput = ' ' + color.wrap(line, color.colors.WHITE, color.styles.bold) + ' ' + finalString + '\n';
-    
-    }
+  return tr;
 
-    if (filename != currentFile) {
-      fileCount = 1;
-      currentFile = filename;
-      this.queue(fileOut + '\n');
-    } else {
-      if (options.maxcount) fileCount++;
-      if (fileCount > options.maxcount) return
-    }
-    this.queue(toOutput);
-    if (options.justone) tr.queue(null);
-  }
 }
+if (isCli) {
+  c
+    .version('0.0.7')
+    .usage('[options] pattern')
+    .option('-d, --dir <dirname>', 'search through directory | default cwd')
+    .option('-i, --ignorecase', 'ignore regex case')
+    .option('-m, --maxcount <num>', 'only show maximum of <num> results per file', parseInt)
+    .option('-n, --norecurse', 'no subdirectory checking')
+    .option('-v, --invertmatch', 'show non-matching lines')
+    .option('-t, --type <type1[,type2...]>', 'comma separated list of types to limit search to')
+    .option('-T, --notype <type1[,type2...]>', 'comma separated list of types to exclude from search')
+    .option('-1, --justone', 'show only the first result')
+    .option('-C, --nocolor', 'turn colorizing off for results')
+    .parse(process.argv);
 
-function end() {
-  this.queue(null);
+  if (!c.args.length) c.help();
+
+  var settings = {
+    dir: c.dir,
+    ignorecase: c.ignorecase,
+    maxcount: c.maxcount,
+    norecurse: c.norecurse,
+    invertmatch: c.invertmatch,
+    type: c.type,
+    notype: c.notype,
+    justone: c.justone,
+    nocolor: c.nocolor,
+    pattern: c.args[0]
+  };
+
+  rs.push(c.dir ? path.normalize(c.dir) : process.cwd());
+  rs.push(null);
+
+  rs.pipe(dirstream({ onlyFiles: true, noRecurse: c.norecurse })).pipe(filestream()).pipe(wack(settings)).pipe(process.stdout);
 }
-
-c
-  .version('0.0.7')
-  .usage('[options] pattern')
-  .option('-d, --dir <dirname>', 'search through directory | default cwd')
-  .option('-i, --ignorecase', 'ignore regex case')
-  .option('-m, --maxcount <num>', 'only show maximum of <num> results per file', parseInt)
-  .option('-n, --norecurse', 'no subdirectory checking')
-  .option('-v, --invertmatch', 'show non-matching lines')
-  .option('-t, --type <type1 [,type2,...]>', 'comma separated list of types to limit search to')
-  .option('-T, --notype <type1 [,type2,...]>', 'comma separated list of types to exclude from search')
-  .option('-1, --justone', 'show only the first result')
-  .option('-C, --nocolor', 'turn colorizing off for results')
-  .parse(process.argv);
-
-if (c.args.length) {
-  options.regex = c.ignorecase ? new RegExp(c.args[0], "ig") : new RegExp(c.args[0], "g");
-} else {
-  c.help();
-}
-
-options.dir = c.dir || process.cwd();
-options.nocolor = c.nocolor;
-options.justone = c.justone;
-options.maxcount = c.maxcount;
-options.invert = c.invertmatch;
-options.ignorecase = c.ignorecase;
-options.norecurse = c.norecurse;
-options.types = c.type ? c.type.replace(/\s+/, '').split(',') : [];
-options.exclude = c.notype ? c.notype.replace(/\s+/, '').split(',') : [];
-rs.push(options.dir);
-rs.push(null);
-
-rs.pipe(dirstream({ onlyFiles: true, noRecurse: options.norecurse })).pipe(filestream()).pipe(tr).pipe(process.stdout);
 
