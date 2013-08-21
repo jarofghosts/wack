20a947c0398356258c82d1f49343845bed5f8e27
jesse keane <me@jessekeane.me>
2013-08-21 23:00:21 +0400

    0.1.7

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ function streamWack(settings) {
 
 if (isCli) {
   c
-    .version('0.1.6')
+    .version('0.1.7')
     .usage('[options] pattern')
     .option('-d, --dir <dirname>', 'search through directory | default cwd')
     .option('-D, --ignoredir <dir1[,dir2...]>', 'comma separated list of directory names to ignore')
