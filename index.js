a0836708cb4a44ae1934ee9faa1780401bb1779c
jesse keane <me@jessekeane.me>
2013-10-09 20:14:09 -0700

    style changes

--- index.js
+++ index.js
@@ -10,104 +10,121 @@ var c = require('commander'),
     color = require('bash-color'),
     Readable = require('stream').Readable,
     path = require('path'),
-    isCli = (require.main === module);;
+    isCli = (require.main === module);
 
-module.exports = streamWack;
+module.exports = streamWack
 
 function wack(options) {
 
   var currentFile = null,
       fileCount = 0,
-      badChars = /[\x00-\x1F\x80-\x9F]+/;
+      badChars = /[\x00-\x1F\x80-\x9F]+/
 
-  options = options || {};
+  options = options || {}
   
-  if (!options.dir) options.dir = process.cwd();
-  if (!/^\//.test(options.dir)) options.dir = path.normalize(options.dir);
-  options.regex = new RegExp(options.pattern, 'g' + (options.ignorecase ? 'i' : ''));
+  if (!options.dir) options.dir = process.cwd()
+  if (!/^\//.test(options.dir)) options.dir = path.normalize(options.dir)
+  options.regex = new RegExp(options.pattern,
+    'g' + (options.ignorecase ? 'i' : ''))
   
-  tr = through(write);
+  tr = through(write)
+
+  return tr
 
   function write(obj) {
-    if (badChars.test(obj.data)) return;
-    var match = options.regex.exec(obj.data);
+    if (badChars.test(obj.data)) return
+    var match = options.regex.exec(obj.data)
     if ((match && !options.invertmatch) || (!match && options.invertmatch)) {
       var filename = obj.filename.substring(options.dir.length),
           line = obj.line + ':',
           str = obj.data,
           fileOut = filename,
-          toOutput;
+          toOutput
+
       if (options.nocolor || options.invertmatch) {
-        toOutput = [' ', line, ' ', str, '\n'].join('');
+        toOutput = ['', line, str].join(' ')
       } else {
         fileOut = color.green(filename);
         var finalString = [str.substring(0, match.index),
-                          color.yellow(match[0], true),
-                          str.substring(match.index + match[0].length)].join(''),
-            toOutput = [' ', color.wrap(line, color.colors.WHITE, color.styles.bold), ' ', finalString, '\n'].join('');
+                           color.yellow(match[0], true),
+                           str.substring(match.index + match[0].length)
+                          ].join(''),
+            toOutput = ['',
+                        color.wrap(line,
+                        color.colors.WHITE,
+                        color.styles.bold),
+                        finalString].join(' ')
       
       }
 
       if (filename != currentFile) {
-        fileCount = 1;
-        currentFile = filename;
-        this.queue(fileOut + '\n');
+        fileCount = 1
+        currentFile = filename
+        this.queue(fileOut + '\n')
       } else {
         if (options.maxcount) {
-          fileCount++;
-          if (fileCount > options.maxcount) return;
+          fileCount++
+          if (fileCount > options.maxcount) return
         }
       }
-      this.queue(toOutput);
-      if (options.justone) tr.queue(null);
+      this.queue(toOutput + '\n')
+      if (options.justone) tr.queue(null)
     }
   }
-
-  return tr;
 
 }
 function addExtensionRegexps(extensions) {
   var i = 0,
       l = extensions.length,
-      result = [];
+      result = []
   for (; i < l; ++i) {
     result.push(new RegExp('\\.' + extensions[i] + '$', 'i'));
   }
-  return result;
+  return result
 }
 function streamWack(settings) {
 
   var policeArgs = {},
-      ignoreDirs = ['.git', '.hg', '.svn'];
+      ignoreDirs = ['.git', '.hg', '.svn']
 
-  settings.types = settings.type ? settings.type.replace(/\s+/, '').split(',') : [];
-  settings.exclude = settings.notype ? settings.notype.replace(/\s+/, '').split(',') : [];
+  settings.types = settings.type ?
+    settings.type.replace(/\s+/, '').split(',') : [];
+  settings.exclude = settings.notype ?
+    settings.notype.replace(/\s+/, '').split(',') : [];
 
   if (settings.knowntypes) {
-    policeArgs.verify = [].concat(addExtensionRegexps(type.allExtensions));
+    policeArgs.verify = [].concat(addExtensionRegexps(type.allExtensions))
   }
   if (settings.exclude.length) {
-    policeArgs.exclude = [];
+    policeArgs.exclude = []
     var i = 0,
-        l = settings.exclude.length;
+        l = settings.exclude.length
     for (; i < l; ++i) {
-      policeArgs.exclude = policeArgs.exclude.concat(type.reverseLookup(settings.exclude[i]));
+      policeArgs.exclude = policeArgs.exclude.concat(
+        type.reverseLookup(settings.exclude[i])
+      )
     }
   }
   if (settings.types.length) {
-    policeArgs.verify = policeArgs.verify || [];
+    policeArgs.verify = policeArgs.verify || []
     var j = 0,
-        k = settings.types.length;
+        k = settings.types.length
     for (; j < k; ++j) {
-      policeArgs.verify = policeArgs.verify.concat(type.reverseLookup(settings.types[j]));
+      policeArgs.verify = policeArgs.verify.concat(
+        type.reverseLookup(settings.types[j])
+      )
     }
   }
 
   if (settings.ignoredir) {
-    ignoreDirs = ignoreDirs.concat(settings.ignoredir.replace(/\s+/g, '').split(','));
+    ignoreDirs = ignoreDirs.concat(
+      settings.ignoredir.replace(/\s+/g, '').split(',')
+    )
   }
 
-  return es.pipeline(dirstream({ onlyFiles: true, noRecurse: settings.norecurse, ignore: ignoreDirs }),
+  return es.pipeline(dirstream({ onlyFiles: true,
+                                 noRecurse: settings.norecurse,
+                                 ignore: ignoreDirs }),
                      police(policeArgs),
                      filestream(),
                      wack(settings));
@@ -118,19 +135,23 @@ if (isCli) {
     .version('0.2.2')
     .usage('[options] pattern')
     .option('-d, --dir <dirname>', 'search through directory | default cwd')
-    .option('-D, --ignoredir <dir1[,dir2...]>', 'comma separated list of directory names to ignore')
+    .option('-D, --ignoredir <dir1[,dir2...]>',
+      'comma separated list of directory names to ignore')
     .option('-i, --ignorecase', 'ignore regex case')
     .option('-k, --knowntypes', 'only include known file types')
-    .option('-m, --maxcount <num>', 'only show maximum of <num> results per file', parseInt)
+    .option('-m, --maxcount <num>',
+      'only show maximum of <num> results per file', parseInt)
     .option('-n, --norecurse', 'no subdirectory checking')
     .option('-v, --invertmatch', 'show non-matching lines')
-    .option('-t, --type <type1[,type2...]>', 'comma separated list of types to limit search to')
-    .option('-T, --notype <type1[,type2...]>', 'comma separated list of types to exclude from search')
+    .option('-t, --type <type1[,type2...]>',
+      'comma separated list of types to limit search to')
+    .option('-T, --notype <type1[,type2...]>',
+      'comma separated list of types to exclude from search')
     .option('-1, --justone', 'show only the first result')
     .option('-C, --nocolor', 'turn colorizing off for results')
     .parse(process.argv);
 
-  if (!c.args.length) c.help();
+  if (!c.args.length) c.help()
 
   var settings = {
     dir: c.dir,
@@ -145,13 +166,13 @@ if (isCli) {
     justone: c.justone,
     nocolor: c.nocolor,
     pattern: c.args[0]
-  };
+  }
 
-  var rs = Readable();
+  var rs = Readable()
 
-  rs.push(c.dir ? path.normalize(c.dir) : process.cwd());
-  rs.push(null);
+  rs.push(c.dir ? path.normalize(c.dir) : process.cwd())
+  rs.push(null)
 
-  rs.pipe(streamWack(settings)).pipe(process.stdout);
+  rs.pipe(streamWack(settings)).pipe(process.stdout)
 }
 
